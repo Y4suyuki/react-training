594ef00be4c16c4dac92dc8140718f73210f0deb
ydammatsu <dammatsu.yuta@moneyforward.co.jp>
2021-12-04 17:08:21 +0900

    complete

--- tic-tac-toe-app/src/index.tsx
+++ tic-tac-toe-app/src/index.tsx
@@ -21,6 +21,26 @@ class Square extends React.Component<SquareProps> {
   }
 }
 
+const calculateWinner = (squares: SquareValue[]): SquareValue => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 type BordProps = {};
 type BordState = {
   squares: SquareValue[],
@@ -37,6 +57,9 @@ class Board extends React.Component<BordProps, BordState> {
 
   handleClick(i: number): void {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -54,7 +77,13 @@ class Board extends React.Component<BordProps, BordState> {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
