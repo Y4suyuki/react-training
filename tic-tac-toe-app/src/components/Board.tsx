ecd4d714fb128a6223e375bce5690f0c3eed95e5
ydammatsu <dammatsu.yuta@moneyforward.co.jp>
2021-12-04 17:57:46 +0900

    add history

--- tic-tac-toe-app/src/components/Board.tsx
+++ tic-tac-toe-app/src/components/Board.tsx
@@ -1,56 +1,25 @@
 import React from 'react';
 import { SquareValue } from '../types/SquareValue'
 import { Square } from '../components/Square'
-import { calculateWinner } from '../lib/calculateWinner'
 
-type BordProps = {};
-type BordState = {
-  squares: SquareValue[],
-  xIsNext: boolean
+type BordProps = {
+  squares: SquareValue[];
+  onClick: (i :number) => void;
 };
 
-export class Board extends React.Component<BordProps, BordState> {
-  constructor(props: BordProps) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i: number): void {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
-
+export class Board extends React.Component<BordProps> {
   renderSquare(i: number) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
