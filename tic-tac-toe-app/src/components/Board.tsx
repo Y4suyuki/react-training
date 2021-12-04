704b0556c1bef2f0bafd5603cc4dc199300673f4
ydammatsu <dammatsu.yuta@moneyforward.co.jp>
2021-12-04 17:22:39 +0900

    refactoring

---
+++ tic-tac-toe-app/src/components/Board.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { SquareValue } from '../types/SquareValue'
+import { Square } from '../components/Square'
+import { calculateWinner } from '../lib/calculateWinner'
+
+type BordProps = {};
+type BordState = {
+  squares: SquareValue[],
+  xIsNext: boolean
+};
+
+export class Board extends React.Component<BordProps, BordState> {
+  constructor(props: BordProps) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i: number): void {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
+  renderSquare(i: number) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+};
