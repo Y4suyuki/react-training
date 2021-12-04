8654861667e24f2cda35a46ad8cc5169eb7f5164
ydammatsu <dammatsu.yuta@moneyforward.co.jp>
2021-12-04 17:01:40 +0900

    O X を実装

--- tic-tac-toe-app/src/index.tsx
+++ tic-tac-toe-app/src/index.tsx
@@ -3,55 +3,76 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-type SquareProps = {}
-type SquereState = {
-  value: string | null
-}
-class Square extends React.Component<SquareProps, SquereState> {
-  constructor(props: SquareProps) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
+type SquareValue = 'O' | 'X' | null;
+type SquareProps = {
+  value: SquareValue
+  onClick: VoidFunction
+};
+class Square extends React.Component<SquareProps> {
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
-class Board extends React.Component {
-  renderSquare() {
-    return <Square/>;
+type BordProps = {};
+type BordState = {
+  squares: SquareValue[],
+  xIsNext: boolean
+};
+class Board extends React.Component<BordProps, BordState> {
+  constructor(props: BordProps) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i: number): void {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
+  renderSquare(i: number) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare()}
-          {this.renderSquare()}
-          {this.renderSquare()}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
         </div>
       </div>
     );
