75658fd89cd4f990415f68db4e618f700e578584
ydammatsu <dammatsu.yuta@moneyforward.co.jp>
2021-12-10 17:55:14 +0900

    renderSquare をリファクタ

--- tic-tac-toe-app/src/components/Board.tsx
+++ tic-tac-toe-app/src/components/Board.tsx
@@ -19,21 +19,22 @@ export const Board: React.FC<BoardProps> = ({squares, onClick}) => {
 
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {[...Array(3)].map((_, i) => {
+        return (
+          <div className="board-row" key={i}>
+            {[...Array(3)].map((_, j) => {
+              const index = 3 * i + j;
+              return (
+                <Square
+                  value={squares[index]}
+                  onClick={() => onClick(index)}
+                  key={j}
+                />
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
