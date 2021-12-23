ee6b2691a3cb55391712a1a06ecdf71d1344143f
Yasuyuki Ageishi <ageishi.yasuyuki@moneyforward.co.jp>
2021-12-24 00:08:20 +0900

    usecallbacking

--- tic-tac-toe-app/src/useGame.ts
+++ tic-tac-toe-app/src/useGame.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Mark } from "./types";
 import { calculateWinner } from "./helper";
 
@@ -19,29 +19,35 @@ export const useGame = () => {
     xIsNext: true,
   });
 
-  function makeMove(i: number) {
-    const history = gameS.history.slice(0, gameS.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
+  const makeMove = useCallback(
+    (i: number) => {
+      const history = gameS.history.slice(0, gameS.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
 
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = gameS.xIsNext ? "X" : "O";
-    setGameS({
-      history: history.concat({ squares: squares }),
-      stepNumber: history.length,
-      xIsNext: !gameS.xIsNext,
-    });
-  }
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = gameS.xIsNext ? "X" : "O";
+      setGameS({
+        history: history.concat({ squares: squares }),
+        stepNumber: history.length,
+        xIsNext: !gameS.xIsNext,
+      });
+    },
+    [gameS, setGameS]
+  );
 
-  function jumpTo(step: number) {
-    setGameS({
-      history: gameS.history.slice(0, step + 1),
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
-  }
+  const jumpTo = useCallback(
+    (step: number) => {
+      setGameS({
+        history: gameS.history.slice(0, step + 1),
+        stepNumber: step,
+        xIsNext: step % 2 === 0,
+      });
+    },
+    [gameS, setGameS]
+  );
 
   const history = gameS.history;
   const current = history[gameS.stepNumber];
