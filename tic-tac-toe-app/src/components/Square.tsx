77a9dc1a1d6fa856b44886c9bfebba33bd82f19b
Yasuyuki Ageishi <ageishi.yasuyuki@moneyforward.co.jp>
2021-12-24 00:08:35 +0900

    watch square is updating and unmounting

--- tic-tac-toe-app/src/components/Square.tsx
+++ tic-tac-toe-app/src/components/Square.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+import { infoStyle, infoStyleRed } from "../constants";
 import { Mark } from "../types";
 
 type SquareProps = { value: Mark; onClick: () => void };
 
 export function Square(props: SquareProps) {
+  useEffect(() => {
+    console.log(`%cupdating square`, infoStyle);
+    return () => {
+      console.log(`%cunmounting square`, infoStyleRed);
+    };
+  }, []);
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
---
+++ tic-tac-toe-app/src/constants.ts
@@ -0,0 +1,4 @@
+const infoColor = "#05AFF2";
+const infoColorRed = "#F24805";
+export const infoStyle = `color: ${infoColor};`;
+export const infoStyleRed = `color: ${infoColorRed};`;
