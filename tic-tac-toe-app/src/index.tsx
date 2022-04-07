61a33fa6ce292310d8faff23b378965b1fb3fb9d
nariura.takuto <50972773+kyoto7250@users.noreply.github.com>
2022-04-07 19:08:02 +0900

    change Square from Components  to function

--- tic-tac-toe-app/src/index.tsx
+++ tic-tac-toe-app/src/index.tsx
@@ -8,14 +8,12 @@ interface SquareProps {
   value: any,
   onClick:() => any
 }
-class Square extends React.Component<SquareProps, {}> {
-  render() {
-    return (
-      <button className="square" onClick={ this.props.onClick }>
-        { this.props.value}
-      </button>
-    );
-  }
+function Square(props: SquareProps) {
+  return (
+    <button className="square" onClick={ props.onClick }>
+      { props.value}
+    </button>
+  );
 }
 
 
