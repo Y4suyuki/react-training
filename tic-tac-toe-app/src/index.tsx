39c650c57ed43f0651d24becc198661bebbfd320
nariura.takuto <50972773+kyoto7250@users.noreply.github.com>
2022-04-07 18:41:10 +0900

    impl for Square setState in onClick

--- tic-tac-toe-app/src/index.tsx
+++ tic-tac-toe-app/src/index.tsx
@@ -21,8 +21,8 @@ class Square extends React.Component<SquareProps, SquareState> {
   }
   render() {
     return (
-      <button className="square" onClick={ () => console.log("click") }>
-        { this.props.value}
+      <button className="square" onClick={ () => this.setState({ value: 'X' })}>
+        { this.state.value}
       </button>
     );
   }
