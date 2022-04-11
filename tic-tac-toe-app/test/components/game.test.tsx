09f726a021ec034d137762e35a5245e44f55a320
nariura.takuto <50972773+kyoto7250@users.noreply.github.com>
2022-04-11 21:59:24 +0900

    adding ui test for game

--- tic-tac-toe-app/test/components/game.test.tsx
+++ tic-tac-toe-app/test/components/game.test.tsx
@@ -1,9 +1,39 @@
 import { Game } from '../../src/components/game'
-import { render } from '@testing-library/react'
+import { render, act } from '@testing-library/react'
 
 describe('Game', () => {
   const game = render(<Game />)
   test('snapshot', () => {
     expect(game).toMatchSnapshot()
+  })
+
+  describe('UI test', () => {
+    test('click button', () => {
+      // changed X in first,
+      const components = render(<Game />)
+      let button  = components.getAllByRole("button")[0]
+      act(() => {
+        button.dispatchEvent(new MouseEvent("click", { bubbles: true}))
+      })
+      expect(button.innerHTML).toBe("X")
+      expect(components.getByText(/Next player: O/i)).toBeTruthy()
+
+      // changed O in second.
+      button  = components.getAllByRole("button")[1]
+      act(() => {
+        button.dispatchEvent(new MouseEvent("click", { bubbles: true}))
+      })
+      expect(button.innerHTML).toBe("O")
+      expect(components.getByText(/Next player: X/i)).toBeTruthy()
+
+      // not changed if click selected button
+      button  = components.getAllByRole("button")[1]
+      act(() => {
+        button.dispatchEvent(new MouseEvent("click", { bubbles: true}))
+      })
+      expect(button.innerHTML).toBe("O")
+      expect(components.getByText(/Next player: X/i)).toBeTruthy()
+
+    })
   })
 })
