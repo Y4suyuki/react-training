7b822d80f757c2db8552e0c52e34bcde634752b6
nariura.takuto <50972773+kyoto7250@users.noreply.github.com>
2022-04-11 16:51:20 +0900

    change Board from class Component to function Component

--- tic-tac-toe-app/src/index.tsx
+++ tic-tac-toe-app/src/index.tsx
@@ -26,37 +26,36 @@ interface BoardState {
   squares: Array<any>
   xIsNext: Boolean
 };
-class Board extends React.Component<BoardProps, BoardState> {
 
-  renderSquare(i: number) {
+
+const Board: React.FunctionComponent<BoardProps> = (props) => {
+
+  function renderSquare(i: number) {
     return <Square
-      value={ this.props.squares[i] }
-      onClick={ () => this.props.onClick(i) }
+      value={ props.squares[i] }
+      onClick={ () => props.onClick(i) }
     />;
   }
 
-  render() {
-
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 interface GameProps {}
